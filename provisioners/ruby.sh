d5183278c12f3a0d31e4bd66009fdce7cc83c39c
David Field <david@thedebonair.net>
2016-01-16 20:52:27 +0000

    Removing in-built vagrant configuration.
    In place of this, a separate repository is used to manage vagrant
    configuration, so only relevant code is included in this repository.

--- provisioners/ruby.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo -e "\033[0;32m >>> Installing Ruby \033[0m"
-
-sudo apt-add-repository ppa:brightbox/ruby-ng
-sudo apt-get update
-sudo apt-get install -qq ruby2.2 ruby2.2-dev
-
-sudo gem install jekyll
